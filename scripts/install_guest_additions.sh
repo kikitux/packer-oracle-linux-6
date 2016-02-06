d74c48743d48c3ad64ca4cf0097a143fdca23695
Alvaro Miranda <kikitux@gmail.com>
2016-02-06 22:58:53 +1300

    Update install_guest_additions.sh

--- scripts/install_guest_additions.sh
+++ scripts/install_guest_additions.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-
-yum install -y gcc kernel-uek-devel
+PACKAGES="gcc kernel-uek-devel kernel-uek-headers perl python-kitchen python-chardet"
+rpm -q ${PACKAGES} &>/dev/null || yum install -y ${PACKAGES}
 
 mkdir -p /mnt/iso
 mount -o loop VBoxGuestAdditions.iso /mnt/iso
 /mnt/iso/VBoxLinuxAdditions.run
 umount /mnt/iso
 rm VBoxGuestAdditions.iso
+
+yum remove -y ${PACKAGES}
+yum autoremove -y
